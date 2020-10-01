d6bd62bcf913a617b008b3af65dbe90b42ff376f
Manal-Jaber <manal.jaber94@gmail.com>
2020-10-01 12:21:44 +0300

    Completed step_1 in javascript-basics3

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,4 @@
+image1 = document.querySelector("#image1");
+image1.addEventListener("mouseover", () =>{
+    image1.setAttribute("style", "border: solid; border-color: red; border-width: 3px;");
+})
