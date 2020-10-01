3fb5872f789d96ab0825fc6194a1ceda48c7f46c
Manal-Jaber <manal.jaber94@gmail.com>
2020-10-01 12:07:01 +0300

    Completed step_5 in javascript-basics2

---
+++ javascript-basics-02/step 5/step_5.js
@@ -0,0 +1,11 @@
+let img = [];
+let id = "";
+for (i=1; i<6; i++) {
+    id = "#image"+i;
+    img[i] = document.querySelector(id);
+    const k=i;
+    img[k].addEventListener("mouseover", () =>{
+        let source = "images/image"+k+"_2.jpg";
+        img[k].src = (source);
+    });
+}
